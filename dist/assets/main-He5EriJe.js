12ea0b1fb6f2a63cb4f6d2e41545670fa8c39fb2
joshua-jatto <j.j.jatto@gmail.com>
2025-07-12 07:52:23 +0100

    vercel build

--- dist/assets/main-He5EriJe.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./logo-B82kq2oO.js";document.getElementById("launchBtn").addEventListener("click",()=>{window.location.href="add-volume.html"}),document.getElementById("nav-logo").src=e;
---
+++ dist/assets/main-vbAavzgT.js
@@ -0,0 +1,1 @@
+import{i as e}from"./logo-DLpWZp6A.js";document.getElementById("launchBtn").addEventListener("click",()=>{window.location.href="add-volume.html"}),document.getElementById("nav-logo").src=e;
