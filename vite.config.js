723cdf2d776069af9a3e773f1af851689050ab2a
joshua-jatto <j.j.jatto@gmail.com>
2025-07-12 08:41:43 +0100

    vercel build

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,20 @@ import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
-  root: 'src', // Project root is src
+  root: '.', // Project root is src
   publicDir: '../public', // Public assets outside src
   base: '/', // Adjust to '/repo-name/' if hosting in a subdirectory
   build: {
     outDir: '/dist', // Output to project_root/dist
     emptyOutDir: true, // Clear dist before building
     assetsDir: 'assets',
+    
     sourcemap: false, // Disable sourcemaps for production
     minify: 'terser', // Use terser for minification
     rollupOptions: {
       input: {
-        main: '/index.html', // Entry point 1
-        addVolume: '/add-volume.html' // Entry point 2
+        main: './index.html', // Entry point 1
+        addVolume: './add-volume.html' // Entry point 2
       }
     }
   },
