803ff270d7e52a2c176ac6ceb84b55cf2337c1ec
joshua-jatto <j.j.jatto@gmail.com>
2025-07-12 12:24:36 +0100

    direct nav to add-volume page

--- dist/assets/main-vbAavzgT.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./logo-DLpWZp6A.js";document.getElementById("launchBtn").addEventListener("click",()=>{window.location.href="add-volume.html"}),document.getElementById("nav-logo").src=e;
