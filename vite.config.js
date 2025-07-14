9a24649669dc3286b660f11df1d859c3732319b7
joshua-jatto <j.j.jatto@gmail.com>
2025-07-14 18:24:11 +0100

    direct nav to add-volume page

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,14 @@ export default defineConfig({
   root: 'src', // Project root is src
   publicDir: '../public', // Public assets outside src
   base: '/', // Adjust to '/repo-name/' if hosting in a subdirectory
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'src/index.html',
+        addVolume: 'src/add-volume.html',
+      },
+    },
+  },
   
   server: {
     open: '/index.html' // Open index.html in dev
